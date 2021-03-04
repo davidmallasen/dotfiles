66842d9a47e0cc8d30531baa74d2b4538b8d5033
David Mallasen <dmallasen@gmail.com>
2021-03-04 21:13:10 +0100

    Add smart symlinking
    - Option -i interactive asks what to do if the file already exists.
    - Options -b --suffix create a _backup when replacing a file.
    - Option -n treats links to directories as if they where files. This
        avoids creation of new symlinks in the original dotfiles.

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ SYMLINK=(bash_aliases bashrc inputrc vimrc vim tmux.conf)
 
 function linkdotfile {
     file="$1"
-    ln -s ~/dotfiles/$file ~/.$file
+    ln -sibn --suffix=_backup ~/dotfiles/$file ~/.$file
 }
 
 for file in ${SYMLINK[@]}; do
-    linkdotfile $file
     echo "Linking $file"
+    linkdotfile $file
 done
 
 echo "Installation complete"
