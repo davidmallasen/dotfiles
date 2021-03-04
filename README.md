e2d093ce5f77077056bca85ac52f3dad358a5ba3
David Mallasen <dmallasen@gmail.com>
2021-03-04 20:34:36 +0100

    Change vim plugins to git submodules

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Dotfiles
 My dotfiles.
 
+Clone using `git clone --recurse-submodules https://github.com/davidmallasen/dotfiles.git`
 To symlink install run `install.sh`.
--- install.sh
+++ install.sh
@@ -1,34 +1,15 @@
 START_DIR=$(pwd)
 
-FILES=(bash_aliases bashrc inputrc vimrc tmux.conf)
+SYMLINK=(bash_aliases bashrc inputrc vimrc vim tmux.conf)
 
 function linkdotfile {
     file="$1"
     ln -s ~/dotfiles/$file ~/.$file
 }
 
-for file in ${FILES[@]}; do
+for file in ${SYMLINK[@]}; do
     linkdotfile $file
     echo "Linking $file"
 done
-
-# Install vim
-if [ ! -d "~/.vim" ]
-then
-    echo "Installing vim"
-
-    mkdir -p ~/.vim/pack/vendor/start/
-
-    linkdotfile vim/colors
-
-    cd ~/.vim/pack/vendor/start/
-    git clone https://github.com/preservim/nerdtree.git
-    git clone https://github.com/vhda/verilog_systemverilog.vim.git
-    git clone https://github.com/kylelaker/riscv.vim.git
-    git clone https://github.com/plasticboy/vim-markdown.git
-    cd $START_DIR
-
-    echo "Installing vim complete"
-fi
 
 echo "Installation complete"
