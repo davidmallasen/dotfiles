ba313d5c22e48203c05a0270a163eb7999b68ef6
David Mallasen <dmallase@ucm.es>
2024-03-26 12:12:25 +0100

    Add fzf fuzzy finder to installation

--- install.sh
+++ install.sh
@@ -15,4 +15,8 @@ done
 # Install vim Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Install fuzzy finder fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 echo "Installation complete. Enter vim and type :PluginInstall"
